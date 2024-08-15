c24709117bae3e0f8b1392eb83fa9c2ca39c8c44
Dave Gentilli <dgentilli@gmail.com>
2024-08-15 11:56:35 +0300

    Initial commit

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+} from 'react-native-reanimated';
+
+const SIZE = 100.0;
 
 export default function App() {
+  const progress = useSharedValue(0);
+  const reanimatedStyle = useAnimatedStyle(() => {
+    return {
+      opacity: progress.value,
+    };
+  }, []);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Animated.View
+        style={{ height: SIZE, width: SIZE, backgroundColor: 'blue' }}
+      />
+      <StatusBar style='auto' />
     </View>
   );
 }
