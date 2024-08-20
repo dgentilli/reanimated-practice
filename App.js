c76db6a527b930a99ec412d3466a7f42c0a7e570
Dave Gentilli <dgentilli@gmail.com>
2024-08-20 12:02:36 +0300

    Reset file for lesson 4 - interpolate colors

--- App.js
+++ App.js
@@ -1,39 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import Animated, {
-  useAnimatedScrollHandler,
-  useSharedValue,
-} from 'react-native-reanimated';
-import Page from './components/Page';
+import { StyleSheet, Text, View } from 'react-native';
 
-const TITLES = ["What's", 'Up', 'Mobile', 'Devs?'];
+const colors = {
+  dark: {
+    background: '#1e1e1e',
+    circle: '#252525',
+    text: '#f8f8f8',
+  },
+  light: {
+    background: '#f8f8f8',
+    circle: '#ffffff',
+    text: '#1e1e1e',
+  },
+};
+
+const SWITCH_TRACK_COLOR = {
+  true: 'rgba(256, 0, 256, 0.2)',
+  false: 'rgba(0, 0, 0, 0.1)',
+};
 
 export default function App() {
-  const translateX = useSharedValue(0);
-
-  const scrollHandler = useAnimatedScrollHandler((event) => {
-    translateX.value = event.contentOffset.x;
-  });
   return (
-    <Animated.ScrollView
-      horizontal
-      pagingEnabled
-      style={styles.container}
-      scrollEventThrottle={16}
-      onScroll={scrollHandler}
-    >
-      {TITLES.map((title, index) => {
-        return (
-          <Page
-            key={title}
-            title={title}
-            index={index}
-            translateX={translateX}
-          />
-        );
-      })}
-    </Animated.ScrollView>
+    <View style={styles.container}>
+      <Text>hi</Text>
+    </View>
   );
 }
 
@@ -41,5 +32,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
